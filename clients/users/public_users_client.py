d0a8d7b7adad559ff49a5514cfc17826a1f5002f
max1mka-dss <m.nadezhkin@garda.local>
2026-03-05 11:50:30 +0300

    Практика: Написание API клиента. Доработка с добавлением клиента и вывода в код

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -4,7 +4,7 @@ from httpx import Response
 from typing import TypedDict
 from httpx_client import client
 
-class LoginRequestDict (TypedDict):
+class CreateUserRequestDict (TypedDict):
     """
     Описание структуры запроса на создание пользователя.
     """
@@ -16,7 +16,7 @@ class LoginRequestDict (TypedDict):
 
 
 class PublicUsersClient(APIClient):
-    def create_user_api(self,request: LoginRequestDict) -> Response:
+    def create_user_api(self,request: CreateUserRequestDict) -> Response:
         """
         Метод выполняет создание пользователя
         :param request: Словарь с email,password,lastName,firstName,middleName
@@ -25,14 +25,14 @@ class PublicUsersClient(APIClient):
 
         return self.post("/api/v1/users", json=request)
 
-# login_data: LoginRequestDict = {
-#     "email": "tes447@example.com",
-#     "password": "securepass123",
-#     "lastName": "Ivanov",
-#     "firstName": "Maksim",
-#     "middleName": "Ivanovich"
-# }
-# #http_client = httpx.Client(base_url = "http://localhost:8000/")
-# http_client = PublicUsersClient(client =client )
-# response = http_client.create_user_api(login_data)
-# print(response.json())
+login_data: CreateUserRequestDict = {
+    "email": "tes447@example.com",
+    "password": "securepass123",
+    "lastName": "Ivanov",
+    "firstName": "Maksim",
+    "middleName": "Ivanovich"
+}
+#http_client = httpx.Client(base_url = "http://localhost:8000/")
+http_client = PublicUsersClient(client =client )
+response = http_client.create_user_api(login_data)
+print("Response from create user",response.json())
