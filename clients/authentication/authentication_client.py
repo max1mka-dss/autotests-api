80ce06368cd797981ed59cc83c720fdba25c5ea8
max1mka-dss <m.nadezhkin@garda.local>
2026-03-25 14:54:42 +0300

    Применяем Pydantic в AuthenticationClient

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -15,7 +15,7 @@ class AuthenticationClient(APIClient):
         :param request: Словарь с email и password.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/authentication/login", json = request.model_dump_json(by_alias=True))
+        return self.post("/api/v1/authentication/login", json=request.model_dump(by_alias=True))
 
     def refresh_api(self,request: RefreshRequestSchema) -> Response:
         """
@@ -23,7 +23,7 @@ class AuthenticationClient(APIClient):
         :param request: Словарь с refreshToken.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/authentication/refresh", json=request.model_dump_json(by_alias=True))
+        return self.post("/api/v1/authentication/refresh", json=request.model_dump(by_alias=True))
 # Добавили метод login
     def login(self, request: LoginRequestSchema) -> LoginResponseSchema:
         response = self.login_api(request)  # Отправляем запрос на аутентификацию
