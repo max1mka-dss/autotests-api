8de05884fbf6429e40b118aae123bd48ea078db2
max1mka-dss <m.nadezhkin@garda.local>
2026-03-26 10:59:25 +0300

    Валидация JSON schema.Практикуемся в работе с JSON schema

---
+++ pydantic_json_schema_get_user.py
@@ -0,0 +1,39 @@
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
+from tools.fakers import get_random_email
+from tools.assertions.schema import validate_json_schema
+# Инициализируем клиент PublicUsersClient
+public_users_client = get_public_users_client()
+
+# Инициализируем запрос на создание пользователя
+create_user_request = CreateUserRequestSchema(
+    email=get_random_email(),
+    password="string",
+    last_name="string",
+    first_name="string",
+    middle_name="string"
+)
+
+# Отправляем POST запрос на создание пользователя
+create_user_response = public_users_client.create_user(create_user_request)
+print('Create user data:', create_user_response)
+
+# Инициализируем пользовательские данные для аутентификации
+authentication_user = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
+)
+
+# Инициализируем клиент PrivateUsersClient
+private_users_client = get_private_users_client(authentication_user)
+
+# Отправляем GET запрос на получение данных пользователя
+get_user_response = private_users_client.get_user_api(create_user_response.user.id)
+#print('Get user data:', get_user_response)
+
+
+get_user_response_schema = GetUserResponseSchema.model_json_schema()
+
+validate_json_schema(instance=get_user_response.json(), schema =get_user_response_schema)
