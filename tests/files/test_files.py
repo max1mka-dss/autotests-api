e6b828a5d4ef8c9e5eed3e8c5cdbc045d9775cd6
max1mka-dss <stebaslo04@gmail.com>
2026-06-24 14:30:32 +0300

    Настройки автотестов с Pydantic.Практика работы с настройками

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -6,6 +6,7 @@ from clients.errors_schema import ValidationErrorResponseSchema, InternalErrorRe
 from clients.files.files_client import FilesClient
 from clients.files.files_client import CreateFileRequestSchema,CreateFileResponseSchema
 from clients.files.files_schema import GetFileResponseSchema
+from config import settings
 from tools.allure.epics import AllureEpic
 from tools.allure.features import AllureFeature
 from tools.allure.stories import AllureStory
@@ -34,7 +35,7 @@ class TestFiles:
     @allure.severity(Severity.BLOCKER)
     @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_file(self,files_client: FilesClient):
-        request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+        request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = CreateFileResponseSchema.model_validate_json(response.text)
 
@@ -64,7 +65,7 @@ class TestFiles:
     @allure.sub_suite(AllureStory.VALIDATE_ENTITY)
     def test_create_file_with_empty_filename(self, files_client: FilesClient ):
         request = CreateFileRequestSchema(filename = "",
-                                          upload_file="./testdata/files/image.png")
+                                          upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
         assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
@@ -79,7 +80,7 @@ class TestFiles:
     def test_create_file_with_empty_directory(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
             directory="",
-            upload_file="./testdata/files/image.png"
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
